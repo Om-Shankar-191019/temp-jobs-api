8a859ac9d5862671f40d67e5f971c1d23d743618
om shankar <om8003golu@gmail.com>
2023-07-11 10:03:47 +0530

    created job controller

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,3 +1,6 @@
+const { StatusCodes } = require("http-status-codes");
+const Job = require("../models/Jobs");
+
 const getAllJobs = async (req, res) => {
   res.send("get all jobs");
 };
@@ -7,7 +10,9 @@ const getJob = async (req, res) => {
 };
 
 const createJob = async (req, res) => {
-  res.json(req.user);
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
 };
 
 const updateJob = async (req, res) => {
