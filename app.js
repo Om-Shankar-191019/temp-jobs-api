1190e905ccbdc4541a83fae42d3911ed1235d4f0
om shankar <om8003golu@gmail.com>
2023-07-11 07:38:11 +0530

    register controller done

--- app.js
+++ app.js
@@ -3,18 +3,22 @@ require("express-async-errors");
 const express = require("express");
 const app = express();
 
+// connect DB
+const connectDB = require("./db/connect");
+
+// routers
+const authRouter = require("./routes/auth");
+const jobsRouter = require("./routes/jobs");
+
 //error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(express.json());
 
-const connectDB = require("./db/connect");
-
 //routes
-app.get("/api/v1/test", (req, res) => {
-  res.send("the first route running");
-});
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -24,7 +28,7 @@ const PORT = process.env.PORT || 5001;
 const start = async () => {
   try {
     //connectDB
-    connectDB(process.env.MONGO_URI);
+    await connectDB(process.env.MONGO_URI);
     app.listen(PORT, console.log(`server is running at port ${PORT}...`));
   } catch (error) {
     console.log(error);
