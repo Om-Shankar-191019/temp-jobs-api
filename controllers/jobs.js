a5a75303ac7302e064dde8b0062427f9d5f3f56d
om shankar <om8003golu@gmail.com>
2023-07-11 13:20:59 +0530

    getJob controller

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,5 +1,6 @@
 const { StatusCodes } = require("http-status-codes");
 const Job = require("../models/Jobs");
+const { NotFoundError } = require("../errors");
 
 const getAllJobs = async (req, res) => {
   const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
@@ -7,7 +8,16 @@ const getAllJobs = async (req, res) => {
 };
 
 const getJob = async (req, res) => {
-  res.send("get job");
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  const job = await Job.findOne({ _id: jobId, createdBy: userId });
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+  res.status(StatusCodes.OK).json({ job });
 };
 
 const createJob = async (req, res) => {
